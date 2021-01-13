fe73e647eb5a49616c2266e358f37f5041c828d6
hawksFTW <69694786+hawksFTW@users.noreply.github.com>
2021-01-12 17:28:29 -0800

    Asteriod Mechanic

---
+++ Assets/Script/AsteriodDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteriodDeath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 respawnPos;
+
+    void Start()
+    {
+        respawnPos = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            transform.position = respawnPos;
+        }
+    }
+
+}
