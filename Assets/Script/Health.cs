2eced17bb244279c1a527bc5b35b85738f9d5f42
EricLighthall <eric.lighthall@digipen.edu>
2021-01-19 16:05:27 -0800

    Added Slime Alien Enemy
    Includes:
    -bullet damage and enemy health
    -enemy animation
    -enemy movement

---
+++ Assets/Script/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 10;
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void HealthRemover(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if(currentHealth <= 0)
+        {
+            deathEvent();
+        }
+    }
+
+    private void deathEvent()
+    {
+        //Do death stuff here
+        Destroy(gameObject);
+    }
+}
